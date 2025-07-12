7835210c0bd7ad795ea4beb8723e5013b2692c62
imusa654 <imusa@nibss-plc.com.ng>
2025-07-12 22:39:47 +0100

    Revert "Removed web config tempo"
    This reverts commit d41c9afcae47dd8ddfa7ba7b6c73d88ed138e990.

---
+++ src/main/java/com/ai/gemini_chat/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ai.gemini_chat;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://gemini-chat-frontend-jtai134f4-musas-projects-892be9d9.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
